204daad181d70127aac562e8cdbf492ab56c173c
Roman <106022434+BasinRoman@users.noreply.github.com>
2023-02-06 23:47:52 +0300

    fixing errors after login/registration complete

---
+++ BookReader.Domain/ViewModel/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace BookReader.Domain.ViewModel
+{
+	public class LoginViewModel
+	{
+		[Required(ErrorMessage = "Please enter login")]
+		[Display(Name = "Login")]
+		public string Login { get; set; }	
+
+		[Required(ErrorMessage ="Please enter password")]
+		[Display(Name = "Password")]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+	}
+}
--- BookReader/wwwroot/js/CheckLoginExist.js
+++ BookReader/wwwroot/js/CheckLoginExist.js
@@ -6,24 +6,22 @@
         url: "Account/IfLoginExist",
         data: {input_login: login},
        
-       
         success: function (result) {
             var message = $("#LoginInputSpan");
             if (login != "") {
                 if (result.data) {
-                    message.html(login + 'already exist!');
+                    message.html(login + "  " + 'already exist!');
                     message.css("color", "red");
                 }
                 else {
-                    message.html(login + 'is free to use');
+                    message.html(login + "  " + 'is free to use');
                     message.css("color", "green");
                 }
                 setTimeout(8000);
             }
             else {
                 message.html("");
-            }
-            
+            }            
         }
         })
 }
